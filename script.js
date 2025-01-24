959bc3161be3f3bb697709392a989c9e48a09789
Balevicky <balevictorien@yahoo.fr>
2025-01-24 18:06:31 +0000

    modified script.js

--- script.js
+++ script.js
@@ -7,3 +7,33 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 300) {
+    navbar.style.height = "40px";
+  } else {
+    navbar.style.height = "90px";
+  }
+
+  if (window.scrollY > 400) {
+    imgImprovise.style.opacity = "1";
+    imgImprovise.style.transform = "translateX(0px)";
+  } else {
+    imgImprovise.style.opacity = "0";
+    imgImprovise.style.transform = "translateX(-200px)";
+  }
+
+  // console.log(window.innerHeight);
+
+  if (!document.getElementById("popup")) {
+  } else {
+    if (window.scrollY > 1200) {
+      popup.style.opacity = "1";
+      popup.style.transform = "translateX(0px)";
+    }
+  }
+});
+// ============ fermer le pop
+window.addEventListener("click", () => {
+  popup.remove();
+});
